01622c1807f21d3542d5075450b400f3c40db573
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-17 16:42:20 +0300

    add new def

--- quiz/home/views.py
+++ quiz/home/views.py
@@ -37,6 +37,7 @@ def get_quiz(request):
 
         for question_obj in question_objs:
             data.append({
+                "id_u": question_obj.id_u,
                 "category": question_obj.category.category_name,
                 "question": question_obj.question,
                 "marks": question_obj.marks,
