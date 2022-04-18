fd1c665fc03b7bf71114996d82148a553182ed81
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-18 16:36:39 +0300

    добавление путей

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
-    path('quiz/', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index')
+    path('', include('home.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
