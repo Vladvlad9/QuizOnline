4e10245a136dd536c6090ca2533fde5924941a7d
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-18 16:36:35 +0300

    добавление путей

--- quiz/home/urls.py
+++ quiz/home/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
     path('api/get-quiz/', views.get_quiz, name='get_quiz'),
-    path('quiz/', views.quiz, name='quiz')
+    path('quiz/', views.quiz, name='quiz'),
+
+    path("signup/", views.SignUp.as_view(), name="signup"),
 ]
